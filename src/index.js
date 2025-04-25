2f88356eeb583bb490dfc4b1279feebc3c1096b7
roboticsysinfo <roboticsysinfo@gmail.com>
2025-04-25 09:36:55 +0530

    index scss fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'select2/dist/js/select2.min.js';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.scss";
 import { Provider } from 'react-redux';
 import store from "./redux/store";
 import { Toaster } from "react-hot-toast";
